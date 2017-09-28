97c076b599b9664752c3b2fc427621a66d8010aa
Jihye <jihye@Jihyeui-MacBook-Air.local>
2017-09-29 01:40:36 +0900

    fix make graph function

--- project1/ahocorasick.cpp
+++ project1/ahocorasick.cpp
@@ -28,20 +28,23 @@ void AhoCorasick::addWord(vector<string> word){
 	for(vector<string>::iterator it = word.begin(); it != word.end(); it++){
 			this->words.push_back(*it);
 			this->patternLen += it->length();
-			this->patternNum++;
+			//this->patternNum++;
 	}
 
-	del.clear();
 	this->makeGraph();
 }
 
 void AhoCorasick::makeGraph(){
 	int cur_state, final_state = 0;
-	this->init_state = patternNum;
+
+	this->init_state = words.size();
+	this->patternNum = words.size();
 	this->state_num = this->init_state + 1;
 	if(this->cur_size < (this->patternNum + this->patternLen + 1)){
 		this->graph.resize(patternNum + patternLen * 2 + 1, vector<int> (MAX_ALPHA, -1));
 		this->cur_size = patternNum + patternLen * 2 + 1;
+		for (auto &row : graph)
+    		fill(row.begin(), row.end(), -1);
 	} else {
 		for (auto &row : graph)
     		fill(row.begin(), row.end(), -1);
@@ -54,38 +57,30 @@ void AhoCorasick::makeGraph(){
 		cur_state = init_state;
 
 		for(index = 0; index < cur_word.length();){
+			
 			if(graph[cur_state][cur_word[index]-'a'] == -1){
-				break;
-			} else {
-				cur_state = graph[cur_state][cur_word[index]-'a'];
-				index++;
-			}
-		}
-
-		while(index < cur_word.length()){
-			if(index+1 == cur_word.length()){
-				graph[cur_state][cur_word[index]-'a'] = final_state++;
-			} else {
 				graph[cur_state][cur_word[index]-'a'] = state_num;
-				cur_state = state_num++;
+				if(index+1 == cur_word.length()){
+					graph[cur_state][cur_word[index]-'a'] = final_state;
+					swap(graph[state_num], graph[final_state]);
+					final_state++;
+					cur_state = state_num++;
+				} else {
+					cur_state = state_num++;
+				}
+			} else {
+				if(index+1 == cur_word.length()){
+					int next = graph[cur_state][cur_word[index]-'a'];
+					swap(graph[next], graph[final_state]);
+					graph[cur_state][cur_word[index]-'a'] = final_state++;
+				}
+				cur_state = graph[cur_state][cur_word[index]-'a'];
 			}
 			index++;
- 		}
-
+		}
 	}
-	/*
-	statenum = init + 1
-	if cur_size < pattern num + patternlen + 1 => resize
-	loop from start word to end word
-		cur_state = init
-		length = 0
-		loop for find end of graph log
-			if graph[cur_state][cur_word[index]] == -1 => break
-			else cur_state = graph[cur_state][cur_word[index]], length++
-		loop to end of cur word
-			graph[cur_state++][cur_word[index]] = length++
-			if cur_word at end => graph[that word index][cur word end] = length
-	*/
+
+	
 }
 
 vector<string> AhoCorasick::search(string input){
@@ -120,14 +115,6 @@ vector<string> AhoCorasick::search(string input){
 			}
 		}
 	}
-	/*
-	cur = init
-	cur string = ""
-	loop from start of input to end of input
-		cur = graph[cur][input[index]]
-		if( cur < init) => accept, register depth at result[string]
-		else if(cur == -1) start at next input[index], string = ""  ex> string , at r -1, start from i
-	*/
 	return this->result;
 }
 	
@@ -138,8 +125,8 @@ void AhoCorasick::deleteWord(string word){
 	if (position != words.end()) {
 		size_t index = distance(words.begin(), position);
 		del.insert(index);
-    	this->words.erase(position);
-    	this->patternNum--;
+    	//this->words.erase(position);
+    	//this->patternNum--;
 	}
 }
 
